f7b59e04cc721fc2411f36af9e184579d2a5f4af
Noy Robinson <noy.robinson@hibob.io>
2024-09-05 16:32:30 +0300

    OOP exercise fixed after code review

--- src/main/kotlin/com/hibob/kotlinBasics/OOP.kt
+++ src/main/kotlin/com/hibob/kotlinBasics/OOP.kt
@@ -4,15 +4,13 @@ data class Participant(val name: String, val email: String)
 
 open class Location(val street: String, val city: String, val country: String)
 
-class UsLocations() : Location {
+class UsLocations(street: String, city: String, country: String, val zipCode: String) : Location(street, city, country)
 
-}
-
-class UkLocation(val postCode: String) : Location()
+class UkLocation(street: String, city: String, country: String, val postCode: String) : Location(street, city, country)
 
 open class Meeting(val name: String, val location: Location, val participants: List<Participant>) {
-    fun addParticipantToMeeting(participant: Participant) {
-        Meeting(name, location, participants + participant)
+    fun addParticipantToMeeting(participant: Participant): Meeting {
+       return Meeting(name, location, participants + participant)
     }
 }
 
