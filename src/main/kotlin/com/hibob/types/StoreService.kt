ea197660cff697dec330c4b211fd47e98a6ff509
Noy Robinson <noy.robinson@hibob.io>
2024-09-05 11:58:53 +0300

    moved public function to top of file to make it easier for the reader to understand the service

--- src/main/kotlin/com/hibob/types/StoreService.kt
+++ src/main/kotlin/com/hibob/types/StoreService.kt
@@ -5,32 +5,28 @@ import java.time.LocalDate
 
 @Component
 class StoreService {
+    fun pay(cart: List<Cart>, payment: Payment): Map<String, Check> {
+        return cart.associate { it ->
+            it.clientId to checkout(it, payment)
+        }
+    }
+
+    private fun checkout(cart: Cart, payment: Payment): Check {
+        val productsWithCustomTrue = cart.products.filter { checkProductCustom(it.custom) }
+        val total = calculateTotal(productsWithCustomTrue)
+        val paymentValid = checkPayment(payment, total)
+
+        if (paymentValid == Statuses.FAILURE)
+            return Check(cart.clientId, paymentValid, 0.0)
+
+        return Check(cart.clientId, paymentValid, total)
+    }
 
     private fun checkProductCustom(custom: Any): Boolean =
         (custom as? Boolean) ?: false
 
-    private fun fail(message: String): Nothing {
-        throw IllegalStateException(message)
-    }
-
     private fun calculateTotal(products: List<Product>): Double =
         products.map { it.price }.sum()
-
-    private fun isCreditCardValid(payment: Payment.CreditCard, price: Double): Statuses  {
-        if (isSupportedType(payment.type) && isCardDetailsValid(payment) && isNotOverTheLimit(payment, price))
-            return Statuses.SUCCESS
-        return Statuses.FAILURE
-    }
-
-    private fun isSupportedType(cardType: CreditCardType): Boolean =
-         cardType == CreditCardType.VISA || cardType == CreditCardType.MASTERCARD
-
-    private fun isCardDetailsValid(payment: Payment.CreditCard): Boolean =
-        payment.expiryDate.isAfter(LocalDate.now()) && payment.number.length == 10
-
-    private fun isNotOverTheLimit(payment: Payment.CreditCard, price: Double): Boolean =
-        payment.limit > price
-
 
     private fun checkPayment(payment: Payment, price: Double): Statuses {
         return when (payment) {
@@ -47,20 +43,23 @@ class StoreService {
         }
     }
 
-    private fun checkout(cart: Cart, payment: Payment): Check {
-        val productsWithCustomTrue = cart.products.filter { checkProductCustom(it.custom) }
-        val total = calculateTotal(productsWithCustomTrue)
-        val paymentValid = checkPayment(payment, total)
-
-        if (paymentValid == Statuses.FAILURE)
-            return Check(cart.clientId, paymentValid, 0.0)
-
-        return Check(cart.clientId, paymentValid, total)
+    private fun fail(message: String): Nothing {
+        throw IllegalStateException(message)
     }
 
-    fun pay(cart: List<Cart>, payment: Payment): Map<String, Check> {
-        return cart.associate { it ->
-            it.clientId to checkout(it, payment)
-        }
+
+    private fun isCreditCardValid(payment: Payment.CreditCard, price: Double): Statuses  {
+        if (isSupportedType(payment.type) && isCardDetailsValid(payment) && isNotOverTheLimit(payment, price))
+            return Statuses.SUCCESS
+        return Statuses.FAILURE
     }
+
+    private fun isSupportedType(cardType: CreditCardType): Boolean =
+         cardType == CreditCardType.VISA || cardType == CreditCardType.MASTERCARD
+
+    private fun isCardDetailsValid(payment: Payment.CreditCard): Boolean =
+        payment.expiryDate.isAfter(LocalDate.now()) && payment.number.length == 10
+
+    private fun isNotOverTheLimit(payment: Payment.CreditCard, price: Double): Boolean =
+        payment.limit > price
 }
