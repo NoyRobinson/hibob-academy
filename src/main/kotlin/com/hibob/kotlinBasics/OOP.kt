e1a7f893b17b963e4f86566e5b76e514c7833d5d
Noy Robinson <noy.robinson@hibob.io>
2024-09-05 13:34:39 +0300

    fixing..

--- src/main/kotlin/com/hibob/kotlinBasics/OOP.kt
+++ src/main/kotlin/com/hibob/kotlinBasics/OOP.kt
@@ -2,17 +2,13 @@ package com.hibob.kotlinBasics
 
 data class Participant(val name: String, val email: String)
 
-abstract class Location {
-    abstract val street: String
-    abstract val city: String
-    abstract val country: String
+open class Location(val street: String, val city: String, val country: String)
+
+class UsLocations() : Location {
+
 }
 
-class UsLocations(override val street: String, override val city: String,
-                 override val country: String, val zipCode: String) : Location()
-
-class UkLocation(override val street: String, override val city: String,
-                 override val country: String, val postCode: String) : Location()
+class UkLocation(val postCode: String) : Location()
 
 open class Meeting(val name: String, val location: Location, val participants: List<Participant>) {
     fun addParticipantToMeeting(participant: Participant) {
