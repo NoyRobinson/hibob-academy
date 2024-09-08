d0d0dbc3d45a605f17a17cd640de3817b5725620
Noy Robinson <noy.robinson@hibob.io>
2024-09-08 15:46:24 +0300

    SQL 1 exercise

--- src/main/resources/db/migration/V202409081401__create-pet.sql
+++ src/main/resources/db/migration/V202409081401__create-pet.sql
@@ -11,16 +11,16 @@ CREATE INDEX idx_pets_company_id ON pets(company_id);
 
 -- INSERT INTO pets (name, type, company_id, date_of_arrival)
 -- VALUES ('Angie', 'dog', '50fcdfda-b823-47e6-9bad-541e87163c9a', '2010-05-14');
---
+
 -- INSERT INTO pets (name, type, company_id, date_of_arrival)
 -- VALUES ('Nessy', 'dog', 'fd320d7a-a288-4f84-9b0a-4648ad40f50e', '2004-08-20');
---
+
 -- INSERT INTO pets (name, type, company_id, date_of_arrival)
 -- VALUES ('Max', 'cat', '50fcdfda-b823-47e6-9bad-541e87163c9a', '2010-05-10');
---
+
 -- INSERT INTO pets (name, type, company_id, date_of_arrival)
 -- VALUES ('Spot', 'rabbit', '50fcdfda-b823-47e6-9bad-541e87163c9a', '2024-09-01');
---
+
 -- INSERT INTO pets (name, type, company_id, date_of_arrival)
 -- VALUES ('Charlie', 'bird', '8ba5f6aa-b0d8-45b8-93a3-78429bcb1ee5', '2024-09-08');
 
