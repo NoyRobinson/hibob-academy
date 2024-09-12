a2527e35f1443677e87e35676b56ebe71f29b7b0
Noy Robinson <noy.robinson@hibob.io>
2024-09-12 15:14:09 +0300

    working on tests

--- src/test/kotlin/com/hibob/academy/dao/OwnerDaoTest.kt
+++ src/test/kotlin/com/hibob/academy/dao/OwnerDaoTest.kt
@@ -27,7 +27,7 @@ class OwnerDaoTest @Autowired constructor(private val sql: DSLContext){
         val ownerName = "Noy"
         val ownerCompanyId = companyId
         val ownerEmployeeId = "123"
-        ownerDao.createOwner(id, ownerName, ownerCompanyId, ownerEmployeeId)
+        ownerDao.createOwner(ownerName, ownerCompanyId, ownerEmployeeId)
         val expected = listOf(OwnerData(id, ownerName, ownerCompanyId, ownerEmployeeId))
         assertEquals(expected, ownerDao.getOwner(ownerCompanyId))
     }
@@ -37,50 +37,51 @@ class OwnerDaoTest @Autowired constructor(private val sql: DSLContext){
         val ownerName1 = "Noy"
         val ownerCompanyId1 = companyId
         val ownerEmployeeId1 = "123"
-        ownerDao.createOwner(id,ownerName1, ownerCompanyId1, ownerEmployeeId1)
+        ownerDao.createOwner(ownerName1, ownerCompanyId1, ownerEmployeeId1)
 
         val ownerName2 = "Tom"
-        ownerDao.createOwner(id, ownerName2, ownerCompanyId1, ownerEmployeeId1)
+        ownerDao.createOwner(ownerName2, ownerCompanyId1, ownerEmployeeId1)
 
         val expected = listOf(OwnerData(id, ownerName1, ownerCompanyId1, ownerEmployeeId1))
         val actual = ownerDao.getOwner(ownerCompanyId1)
         assertEquals(expected, actual)
     }
 
-//    @Test
-//    fun `get owner information by pet id`(){
-//
-//        val ownerName = "Noy"
-//        val ownerCompanyId = companyId
-//        val ownerEmployeeId = "123"
-//        ownerDao.createOwner(id, ownerName, ownerCompanyId, ownerEmployeeId)
-//
-//        val petId = 2L
-//        val petName = "Angie"
-//        val petTypeString = "Dog"
-//        val petType = PetType.convertStringToPetType(petTypeString)
-//        val petCompanyId = companyId
-//        val dateOfArrival = Date.valueOf("2010-05-20")
-//        createPet(petId, petName, petTypeString, petCompanyId, dateOfArrival, id)
-//
-//        val expected = OwnerData(id, ownerName, ownerCompanyId, ownerEmployeeId)
-//        val actual = ownerDao.getOwnerByPetId(petId)
-//        assertEquals(expected, actual)
-//    }
-//
-//    fun `try get information of owner by pet id for a pet that doesnt have an owner`(){
-//
-//        val petId = 2L
-//        val petName = "Angie"
-//        val petTypeString = "Dog"
-//        val petType = PetType.convertStringToPetType(petTypeString)
-//        val petCompanyId = companyId
-//        val dateOfArrival = Date.valueOf("2010-05-20")
-//        createPet(petId, petName, petTypeString, petCompanyId, dateOfArrival, OL)
-//
-//        val actual = ownerDao.getOwnerByPetId(petId)
-//        assertEquals(null, actual)
-//    }
+    // fix 2 tests under
+    @Test
+    fun `get owner information by pet id`(){
+
+        val ownerName = "Noy"
+        val ownerCompanyId = companyId
+        val ownerEmployeeId = "123"
+        ownerDao.createOwner(id, ownerName, ownerCompanyId, ownerEmployeeId)
+
+        val petId = 2L
+        val petName = "Angie"
+        val petTypeString = "Dog"
+        val petType = PetType.convertStringToPetType(petTypeString)
+        val petCompanyId = companyId
+        val dateOfArrival = Date.valueOf("2010-05-20")
+        createPet(petId, petName, petTypeString, petCompanyId, dateOfArrival, id)
+
+        val expected = OwnerData(id, ownerName, ownerCompanyId, ownerEmployeeId)
+        val actual = ownerDao.getOwnerByPetId(petId)
+        assertEquals(expected, actual)
+    }
+
+    fun `try get information of owner by pet id for a pet that doesnt have an owner`(){
+
+        val petId = 2L
+        val petName = "Angie"
+        val petTypeString = "Dog"
+        val petType = PetType.convertStringToPetType(petTypeString)
+        val petCompanyId = companyId
+        val dateOfArrival = Date.valueOf("2010-05-20")
+        createPet(petId, petName, petTypeString, petCompanyId, dateOfArrival, OL)
+
+        val actual = ownerDao.getOwnerByPetId(petId)
+        assertEquals(null, actual)
+    }
 
     @BeforeEach
     @AfterEach
