3dc258b8c31e2fc741044e38e9b270df8111124a
Noy Robinson <noy.robinson@hibob.io>
2024-09-15 12:21:10 +0300

    added an object of owner and pet with no id - changed tests to work with object instead of all parameters.
    added API for pet and owner
    OwnerResource
    OwnerService
    PetResource
    PetService

--- src/main/kotlin/com/hibob/academy/dao/models.kt
+++ src/main/kotlin/com/hibob/academy/dao/models.kt
@@ -6,7 +6,11 @@ data class Example(val id: Long, val companyId: Long, val data: String)
 
 data class OwnerData(val id: Long, val name: String, val companyId: Long, val employeeId: String)
 
+data class Owner(val name: String, val companyId: Long, val employeeId: String)
+
 data class PetData(val id: Long, var name: String, val type: PetType, val companyId: Long, val dateOfArrival: Date, val ownerId: Long?)
+
+data class Pet(var name: String, val type: PetType, val companyId: Long, val dateOfArrival: Date, val ownerId: Long?)
 
 enum class PetType {
     DOG, CAT, BIRD;
