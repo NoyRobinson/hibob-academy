eaf484746430df4eb2b42447c3dac6e86580a9c4
Noy Robinson <noy.robinson@hibob.io>
2024-09-15 22:15:35 +0300

    changed from QueryParam to PathParam
    fixed after review
    
    added API for pet for functions countPetsByType and getPetsByOwner

--- src/main/kotlin/com/hibob/academy/resource/PetResource.kt
+++ src/main/kotlin/com/hibob/academy/resource/PetResource.kt
@@ -44,13 +44,6 @@ import org.springframework.web.bind.annotation.RequestBody
         }
 
         @GET
-        @Path("company/{companyId}/allPets")
-        fun getAllPets(@PathParam("companyId") companyId: Long): Response {
-            val listOfPets = petService.getAllPets(companyId)
-            return Response.ok(listOfPets).build()
-        }
-
-        @GET
         @Path("company/{companyId}{petId}")
         fun getPetById(@PathParam("petId") petId: Long, @PathParam("companyId") companyId: Long): Response {
             val pet = petService.getPetById(petId, companyId)
