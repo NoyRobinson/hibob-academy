7031350552873ccbf52ebc55be9657b157c9b38b
Noy Robinson <noy.robinson@hibob.io>
2024-09-16 14:37:18 +0300

    fixed review

--- src/main/kotlin/com/hibob/academy/resource/PetResource.kt
+++ src/main/kotlin/com/hibob/academy/resource/PetResource.kt
@@ -49,6 +49,13 @@ import org.springframework.web.bind.annotation.RequestBody
             return Response.ok().entity(listOfPets).build()
         }
 
+        @GET
+        @Path("/{petId}")
+        fun getPetById(@PathParam("petId") petId: Long, @QueryParam("companyId") companyId: Long): Response {
+            val pet = petService.getPetById(petId, companyId)
+            return Response.ok().entity(pet).build()
+        }
+
         @DELETE
         @Path("/{petId}")
         fun deletePet(@PathParam("petId") petId: Long, @QueryParam("companyId") companyId: Long): Response {
