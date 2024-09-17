b5f5ce95002cdf6b863275ab0c2530aed40faa41
Noy Robinson <noy.robinson@hibob.io>
2024-09-17 12:37:19 +0300

    removed function that doesn't belong to the pr

--- src/main/kotlin/com/hibob/academy/service/PetService.kt
+++ src/main/kotlin/com/hibob/academy/service/PetService.kt
@@ -43,11 +43,4 @@ class PetService @Inject constructor(private val petDao: PetDao) {
 
     fun countPetsByType(companyId: Long) =
         petDao.countPetsByType(companyId)
-
-    fun adoptPets(ownerId: Long, companyId: Long, petsIds: List<Long>): Map<Long, Boolean> {
-        return petsIds.associate { petId ->
-            val success = petDao.updatePetOwner(petId, ownerId, companyId)
-            petId to (success == 1)
-        }
-    }
 }
