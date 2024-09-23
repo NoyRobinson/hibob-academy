a664ef324883f5ffeedb5bfafebbab1f781b590b
Noy Robinson <noy.robinson@hibob.io>
2024-09-23 19:21:29 +0300

    added feedback service

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/service/FeedbackService.kt
@@ -0,0 +1,51 @@
+package com.hibob.academy.employeeFeedback.service
+
+import com.hibob.academy.employeeFeedback.dao.AnonymityType
+import com.hibob.academy.employeeFeedback.dao.FeedbackDao
+import com.hibob.academy.employeeFeedback.dao.FeedbackForSubmission
+import com.hibob.academy.employeeFeedback.dao.FeedbackStatus
+import jakarta.ws.rs.BadRequestException
+import org.springframework.stereotype.Service
+
+@Service
+class FeedbackService(private val feedbackDao: FeedbackDao) {
+    fun submitFeedback(employeeId: Int, anonymity: AnonymityType, feedback: String): Boolean {
+        val employeeInfo = employeeDao.getInfoById(employeeId)
+
+        var employeeIdForFeedback: Int? = employeeId
+        if (anonymity == AnonymityType.ANONYMOUS)
+            employeeIdForFeedback = null
+
+        if (feedback.length < 30 || feedback.length > 500)
+            return false
+
+        val feedbackForSubmission =
+            FeedbackForSubmission(employeeIdForFeedback, employeeInfo.companyId, anonymity, feedback)
+        feedbackDao.submitFeedback(feedbackForSubmission)
+        return true
+    }
+
+    fun viewAllSubmittedFeedback(employeeId: Int): Boolean {
+        val employeeInfo = employeeDao.getInfoById(employeeId)
+        if (employeeInfo.role != "ADMIN" && employeeInfo.role != "HR")
+            return false
+        feedbackDao.viewAllSubmittedFeedback(employeeInfo.companyId)
+        return true
+    }
+
+    fun viewStatusOfMyFeedback(employeeId: Int, feedbackId: Int): Boolean {
+        val employeeInfo = employeeDao.getInfoById(employeeId)
+        val feedbackInfo = feedbackDao.getFeedback(feedbackId)
+        if (feedbackInfo.employeeId != employeeId)
+            throw BadRequestException("Unauthorized to view this feedback status")
+        val feedbackStatus = FeedbackStatus(employeeInfo.companyId, employeeId, feedbackId)
+        return feedbackDao.viewStatusOfMyFeedback(feedbackStatus)
+    }
+
+    fun viewStatusesOfAllMySubmittedFeedback(companyId: Int, employeeId: Int) {
+        val employeeInfo = employeeDao.getInfoById(employeeId)
+        if (employeeInfo.companyId != companyId)
+            throw BadRequestException("Unauthorized to view feedback of a different company")
+        feedbackDao.viewStatusesOfAllMySubmittedFeedback(employeeInfo.companyId, employeeInfo.employeeId)
+    }
+}
