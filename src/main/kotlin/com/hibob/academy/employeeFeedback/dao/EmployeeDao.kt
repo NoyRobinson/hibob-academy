a1a404b2f34febebe6a582d60654585a3e582f1b
NoyRobinson <118342711+NoyRobinson@users.noreply.github.com>
2024-09-24 09:26:58 +0300

    Noy/EmployeeDao (#53)
    * added employee dao
    
    * added implementation for EmployeeDao + tests
    
    * remove file
    
    * fix employeeDao
    
    * remove unused import
    
    ---------
    
    Co-authored-by: Noy Robinson <noy.robinson@hibob.io>

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/dao/EmployeeDao.kt
@@ -0,0 +1,30 @@
+package com.hibob.academy.employeeFeedback.dao
+
+import jakarta.ws.rs.BadRequestException
+import org.jooq.DSLContext
+import org.jooq.RecordMapper
+import org.jooq.Record
+import org.springframework.stereotype.Repository
+
+@Repository
+class EmployeeDao(private val sql: DSLContext) {
+    private val employeeTable = EmployeeTable.instance
+
+    private val employeeMapper = RecordMapper<Record, EmployeeInfo> { record ->
+        EmployeeInfo(
+            record[employeeTable.id],
+            RoleType.convertStringToRoleType(record[employeeTable.role]),
+            record[employeeTable.department],
+            record[employeeTable.companyId]
+        )
+    }
+
+    fun getEmployeeById(id: Int): EmployeeInfo {
+        val employee = sql.select(employeeTable.id, employeeTable.role, employeeTable.department, employeeTable.companyId)
+            .from(employeeTable)
+            .where(employeeTable.id.eq(id))
+            .fetchOne(employeeMapper)
+
+        return employee ?: throw BadRequestException("Employee not found")
+    }
+}
---
+++ src/test/kotlin/com/hibob/academy/employeeFeedback/dao/EmployeeDaoTest.kt
@@ -0,0 +1,20 @@
+package com.hibob.academy.employeeFeedback.dao
+
+import com.hibob.academy.utils.BobDbTest
+import jakarta.ws.rs.BadRequestException
+import org.jooq.DSLContext
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+
+@BobDbTest
+class EmployeeDaoTest @Autowired constructor(private val sql: DSLContext) {
+    private val employeeDao = EmployeeDao(sql)
+    private val employeeTable = EmployeeTable.instance
+
+    @Test
+    fun `Employee with this id doesn't exist`(){
+        assertThrows<BadRequestException>{ employeeDao.getEmployeeById(100) }
+    }
+}
