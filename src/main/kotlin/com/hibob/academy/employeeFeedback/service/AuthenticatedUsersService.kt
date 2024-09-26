80dff9032c3caf539eff74d7c7a4383eb29a02e0
Noy Robinson <noy.robinson@hibob.io>
2024-09-26 15:08:48 +0300

    fixed after review

--- src/main/kotlin/com/hibob/academy/employeeFeedback/service/AuthenticatedUsersService.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/service/AuthenticatedUsersService.kt
@@ -37,11 +37,4 @@ class AuthenticatedUsersService {
     fun validateRole(role: RoleType, validRoles: List<RoleType>) {
         if (!validRoles.contains(role)) throw NotAuthorizedException("Unauthorized role")
     }
-
-    fun convertToAnonymityType(anonymity: String?): AnonymityType? {
-        anonymity?.let{
-            return convertStringToAnonymityType(anonymity)
-
-        } ?: return null
-    }
 }
